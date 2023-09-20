72a4276455d29de67968ac8dbae49a9a855ac5ea
agustinazn <101346456+agustinazn@users.noreply.github.com>
2023-09-20 21:13:50 +0200

    Update newtonmethod1D.m

--- newtonmethod1D.m
+++ newtonmethod1D.m
@@ -1,21 +1,19 @@
-function [x_star, j] = newtonmethod1D(x_0, T, S, r, K, epsilon)
-sigma = 1;
-d_1 = (log(S/K) + (r+0.5*sigma*sigma)*T)/(sigma*sqrt(T));
-d_2 = d_1 - sigma * sqrt(T);
-d1 = @(sigma) d_1;
-d2 = @(sigma) d_2;
+function [sigma_star, iter] = newtonmethod1D(sigma_0, T, S, r, K, epsilon)
 
-f = @(sigma) S*normcdf(d1(sigma)) - K*exp(-r*T)*normcdf(d2(sigma));
+d1 = @(sigma) (log(S/K) + (r+0.5*sigma^2)*T)/(sigma*sqrt(T));
+d2 = @(sigma) d1(sigma) - sigma * sqrt(T);
 
-f_prime = @(sigma) S*((exp(-d1(sigma)^2/2))/(sqrt(2*pi)))*sqrt(T);
+g = @(sigma) S*normcdf(d1(sigma)) - K*exp(-r*T)*normcdf(d2(sigma)) - 10.78;
 
-x_star = x_0;
+g_prime = @(sigma) S*((exp(-d1(sigma)^2/2))/(sqrt(2*pi)))*sqrt(T);
 
-j = 0;
-while abs(f(x_star))>epsilon
-    j = j + 1;
-    x_star = x_star - feval(f,x_star)/feval(f_prime,x_star);
-    x_star
-    f(x_star)
+sigma_star = sigma_0;
+
+iter = 0;
+
+while abs(g(sigma_star))>epsilon
+    iter = iter + 1;
+    sigma_star = sigma_star - g(sigma_star)/g_prime(sigma_star);
 end
+
 end
