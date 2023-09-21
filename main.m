3abca215c527c32ebf7316476e3a804f606026bf
agustinazn <101346456+agustinazn@users.noreply.github.com>
2023-09-21 17:24:23 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+%% Exercise 1
 S = 100; %Current value of the stock, S(0) 
 K = 100; %Strike of the option
 T = 1.5; %Expiration
@@ -6,4 +7,14 @@ sigma_0 = 0.5; %Chosen at random in interval (O,1)
 epsilon = 1e-12; %Tolerance 
 [sigma, iterations] = newtonmethod1D(sigma_0, T, S, r, K, epsilon)
 
+%% Exercise 2a
+x = [3;3];
+y = [2;2];
+[x_star, j] = newtonmethod2D(3, 3, epsilon)
+[y_star, j] = newtonmethod2D(2,2, epsilon)
 
+%% Exercise 2b
+grad_f = @(x) [4*( ...
+    x(1) - 2)^3 + 2*(x(1) - 2*x(2)); -4*(x(1) - 2*x(2))];
+[x_gd_star, k] = cstgradientdescent(grad_f, x, epsilon);
+%[y_gd_star, k] = cstgradientdescent(grad_f, y, epsilon);
