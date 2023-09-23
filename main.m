0f81e3b1dcea9af59882931fe0de43f914959ed8
agustinazn <101346456+agustinazn@users.noreply.github.com>
2023-09-23 14:55:16 +0200

    Update main.m

--- main.m
+++ main.m
@@ -14,7 +14,21 @@ y = [2;2];
 [y_star, j] = newtonmethod2D(2,2, epsilon)
 
 %% Exercise 2b
-grad_f = @(x) [4*( ...
-    x(1) - 2)^3 + 2*(x(1) - 2*x(2)); -4*(x(1) - 2*x(2))];
-[x_gd_star, k] = cstgradientdescent(grad_f, x, epsilon);
-%[y_gd_star, k] = cstgradientdescent(grad_f, y, epsilon);
+grad_f = @(q) [4*(q(1) - 2)^3 + 2*(q(1) - 2*q(2)); -4*(q(1) - 2*q(2))];
+
+x = [3;3];
+y = [2;2];
+
+alpha1 = 0.1;
+alpha2 = 1e-2;
+alpha3 = 1e-3;
+
+epsilon=1e-6;
+
+[x_gd_star_1, a] = cstgradientdescent(grad_f, x, alpha1,epsilon)
+[x_gd_star_2, b] = cstgradientdescent(grad_f, x, alpha2,epsilon)
+[x_gd_star_3, c] = cstgradientdescent(grad_f, x, alpha3,epsilon)
+
+[y_gd_star_1, e] = cstgradientdescent(grad_f, y, alpha1,epsilon)
+[y_gd_star_2, f] = cstgradientdescent(grad_f, y, alpha2,epsilon)
+[y_gd_star_3, g] = cstgradientdescent(grad_f, y, alpha3,epsilon)
